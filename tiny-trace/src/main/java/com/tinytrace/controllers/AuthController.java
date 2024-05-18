ed6b87b58a77d971233f60d3a7787d7f1d89760e
Pang Rui Wei <>
2024-05-18 15:29:15 +0800

    move login and signup to auth controller

---
+++ tiny-trace/src/main/java/com/tinytrace/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.tinytrace.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tinytrace.dto.LoginRequest;
+import com.tinytrace.dto.SignupRequest;
+import com.tinytrace.services.AuthService;
+
+@RestController
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login") 
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        authService.handleLogin(loginRequest);
+        return ResponseEntity.ok().build(); 
+    }    
+
+    @PostMapping("/signup") 
+    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+        authService.handleSignup(signupRequest); 
+        return ResponseEntity.ok().build(); 
+    }
+}
--- tiny-trace/src/main/java/com/tinytrace/controllers/UserController.java
+++ tiny-trace/src/main/java/com/tinytrace/controllers/UserController.java
@@ -3,26 +3,59 @@ package com.tinytrace.controllers;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tinytrace.UserModelAssembler;
+import com.tinytrace.exceptions.UserNotFoundException;
+import com.tinytrace.models.User;
 import com.tinytrace.repositories.UserRepository;
+import com.tinytrace.services.UserService;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserModelAssembler userModelAssembler; 
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository; 
+    public UserController(UserModelAssembler userModelAssembler, UserService userService) {
+        this.userModelAssembler = userModelAssembler;
+        this.userService = userService; 
     }
 
-    @PostMapping("/users")
-    public String login() {
-        // handle logic of login
-        return "login success"; 
+    @GetMapping("/users/{id}")
+    public EntityModel<User> getUserById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return userModelAssembler.toModel(user); 
     }
 
-    @PostMapping("/signup")
-    public String signup() {
-        //handle logic of signup
-        return "signup success";
+    @GetMapping("/users")
+    public CollectionModel<EntityModel<User>> all() {
+        List<EntityModel<User>> users = userService.findAll()
+            .map(userModelAssembler::toModel)
+            .collect(Collectors.toList());
+        return CollectionModel.of(users, linkTo(
+            methodOn(UserController.class).all()).withSelfRel()
+        );
     }
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteById(id); 
+        return ResponseEntity.noContent().build(); 
+    }
+
 
 }
