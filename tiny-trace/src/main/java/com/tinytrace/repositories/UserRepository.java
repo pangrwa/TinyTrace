7c9077bf5912a472297057e354d84b47a229c2f8
Pang Rui Wei <>
2024-05-18 15:29:36 +0800

    add query methods for user repo

--- tiny-trace/src/main/java/com/tinytrace/repositories/UserRepository.java
+++ tiny-trace/src/main/java/com/tinytrace/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.tinytrace.repositories;
 
-import com.tinytrace.entities.User;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
+
+import com.tinytrace.models.User;
 
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    Optional<User> findByUsername(String username); 
+    boolean existsByEmail(String email); 
 }
