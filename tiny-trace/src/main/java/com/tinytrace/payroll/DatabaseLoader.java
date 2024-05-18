2fe2613ae98521118434d5980def680e2e027ba8
Pang Rui Wei <>
2024-05-18 15:30:12 +0800

    add seed user data

--- tiny-trace/src/main/java/com/tinytrace/payroll/DatabaseLoader.java
+++ tiny-trace/src/main/java/com/tinytrace/payroll/DatabaseLoader.java
@@ -4,19 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.tinytrace.models.User;
+import com.tinytrace.repositories.UserRepository;
+
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
-    private final EmployeeRepository repository;
+    private final EmployeeRepository employeeRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public DatabaseLoader(EmployeeRepository repository) {
-        this.repository = repository;
+    public DatabaseLoader(EmployeeRepository employeeRepository, UserRepository userRepository) {
+        this.employeeRepository = employeeRepository;
+        this.userRepository = userRepository; 
     }
 
     @Override
     public void run(String ... strings) throws Exception {
-        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.employeeRepository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.userRepository.save(new User("admin@gmail.com", "admin", "admin123"));
+        this.userRepository.save(new User("worker@gmail.com", "worker", "worker123"));
     }
     
 }
