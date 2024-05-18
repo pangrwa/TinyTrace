5bb82e015b47cb98e9fa99f6f7427905407e6508
Pang Rui Wei <>
2024-05-18 15:29:51 +0800

    remodify exception names

--- tiny-trace/src/main/java/com/tinytrace/exceptions/UserNotFoundException.java
+++ tiny-trace/src/main/java/com/tinytrace/exceptions/UserNotFoundException.java
@@ -3,6 +3,12 @@ package com.tinytrace.exceptions;
 public class UserNotFoundException extends RuntimeException {
     
     public UserNotFoundException(Long id) {
-        super("Could not find user" + id); 
+        super("Could not find user with id: " + id); 
     }
+
+    public UserNotFoundException(String username) {
+        super("Could not find user with username: " + username);
+    }
+
+    
 }
