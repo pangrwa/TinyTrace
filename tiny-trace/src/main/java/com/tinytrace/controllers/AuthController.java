63a384718f7071cff0d76c4dd52966838984b2f6
Pang Rui Wei <>
2024-05-23 16:31:21 +0800

    auth controller handle passing of jwt, create authentication response

--- tiny-trace/src/main/java/com/tinytrace/controllers/AuthController.java
+++ tiny-trace/src/main/java/com/tinytrace/controllers/AuthController.java
@@ -1,31 +1,42 @@
 package com.tinytrace.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tinytrace.configs.SecurityUser;
+import com.tinytrace.dto.AuthenticationResponse;
 import com.tinytrace.dto.LoginRequest;
 import com.tinytrace.dto.SignupRequest;
+import com.tinytrace.models.User;
 import com.tinytrace.services.AuthService;
+import com.tinytrace.services.JwtService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
+@AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
-
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+    private final JwtService jwtService;
 
     @PostMapping("/auth/login") 
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        authService.handleLogin(loginRequest);
-        return ResponseEntity.ok().build(); 
+        User user = authService.handleLogin(loginRequest);
+        UserDetails userDetails = new SecurityUser(user);
+        String jwt = jwtService.generateToken(userDetails);
+        AuthenticationResponse authResponse = new AuthenticationResponse(jwt);
+        return ResponseEntity.ok(authResponse);
     }    
 
     @PostMapping("/auth/signup") 
-    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
-        authService.handleSignup(signupRequest); 
-        return ResponseEntity.ok().build(); 
+    public ResponseEntity<AuthenticationResponse> signup(@RequestBody SignupRequest signupRequest) {
+        User user = authService.handleSignup(signupRequest); 
+        UserDetails userDetails = new SecurityUser(user);
+        String jwt = jwtService.generateToken(userDetails);
+        AuthenticationResponse authResponse = new AuthenticationResponse(jwt); 
+        return ResponseEntity.ok(authResponse);
     }
 }
