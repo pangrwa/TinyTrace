9dbefae7cadc93efbf8afcc9310af2f58c3f705c
Pang Rui Wei <>
2024-05-24 15:14:26 +0800

    cleanup main entry point

--- tiny-trace/src/main/java/com/tinytrace/TinyTraceApplication.java
+++ tiny-trace/src/main/java/com/tinytrace/TinyTraceApplication.java
@@ -1,12 +1,7 @@
 package com.tinytrace;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.SpringVersion;
 
 import io.github.cdimascio.dotenv.Dotenv;
@@ -21,16 +16,4 @@ public class TinyTraceApplication {
 		SpringApplication.run(TinyTraceApplication.class, args);
 		System.out.printf("My Spring version is: %s\n", SpringVersion.getVersion());
 	}
-
-	//@Bean
-	//public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	//	return args -> {
-	//		System.out.println("Let's inspect the beans provided by Spring Boot:");
-	//		String[] beanNames = ctx.getBeanDefinitionNames();
-	//		Arrays.sort(beanNames);
-	//		for (String beanName : beanNames) {
-	//			System.out.println(beanName);
-	//		}
-	//	};
-	//} 
 }
