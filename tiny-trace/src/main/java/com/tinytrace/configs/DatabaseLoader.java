4da32bcd1e2cd7d881fdeb726098376f397af7b1
Pang Rui Wei <>
2024-05-24 15:20:33 +0800

    adjust database loader to clean slate mongodb db, remove payroll folder

---
+++ tiny-trace/src/main/java/com/tinytrace/configs/DatabaseLoader.java
@@ -0,0 +1,20 @@
+package com.tinytrace.payroll;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        mongoTemplate.getDb().drop();
+    }
+
+}
