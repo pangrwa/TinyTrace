1966dfae4115522b0233d4d38c223a19353e8e27
Pang Rui Wei <>
2024-06-03 18:14:39 +0800

    create timeout for token expiry

--- frontend/src/routes/PrivateRouter.js
+++ frontend/src/routes/PrivateRouter.js
@@ -1,12 +1,44 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useUrl } from "../contexts/UrlContext";
+import { useJwt } from "react-jwt";
 
 
 export default function PrivateRoute() {
     const { token } = useAuth();
+    const { dispatch } = useUrl(); 
+    const navigate = useNavigate();  
+    const { decodedToken, isExpired } = useJwt(token);
 
-    if (!token) {
+    useEffect(() => {
+        let timerRef = null; 
+
+        const onExpire = () => {
+            navigate("/"); 
+        }
+
+        // seems sketchy 
+        if (!decodedToken) {
+            return; 
+        }
+        
+        const currentTime = (new Date()).getTime(); 
+        const expiryTime = decodedToken.exp * 1000 - currentTime; 
+
+        if (expiryTime > 0) {
+            timerRef = setTimeout(onExpire, expiryTime);  
+        } else {
+            onExpire(); 
+        }
+        
+        return () => {
+            clearTimeout(timerRef); 
+        }
+    }, [dispatch, token])
+
+    if (!token || isExpired) {
         return <Navigate to="/login" />
     }
     return <Outlet />
